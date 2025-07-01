5e076d3527f02ad1f516ba95ec739dccbdaf3013
João Paulo Paggi <nullifidianz@gmail.com>
2025-07-01 15:13:59 -0300

    Adiciona arquivos iniciais ao repositório

---
+++ src/main/java/com/nullifidianz/md_notes/MdNotesApplication.java
@@ -0,0 +1,13 @@
+package com.nullifidianz.md_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nullifidianz/md_notes/MdNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nullifidianz.md_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
